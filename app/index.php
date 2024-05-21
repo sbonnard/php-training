2eb241f1864d109556a101fa2f7a28800ebc0f0f
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-05-21 11:25:40 +0200

    Ex 1,2, 3 done, blocking a little on 4 because of float ?

--- app/index.php
+++ app/index.php
@@ -28,13 +28,16 @@
         <!-- QUESTION 1 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 1</h2>
-            <p class="exercice-txt">Ecrivez la phrase suivante dans une balise HTML P en utilisant les 2 variables ci-dessous :</p>
+            <p class="exercice-txt">Ecrivez la phrase suivante dans une balise HTML P en utilisant les 2 variables
+                ci-dessous :</p>
             <p class="exercice-txt">"<i>Firstname</i> a obtenu <i>score</i> points à cette partie."</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $firstname = "Michel";
                 $score = 327;
-            ?>
+
+                echo "<p>$firstname a obtenu $score points à cette partie.</p>"
+                    ?>
             </div>
         </section>
 
@@ -44,27 +47,40 @@
             <h2 class="exercice-ttl">Question 2</h2>
             <p class="exercice-txt">Afficher dans une liste HTML le nom des produits suivants et leurs prix.</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $nameProduct1 = "arc";
                 $priceProduct1 = 10.30;
                 $nameProduct2 = "flèche";
                 $priceProduct2 = 2.90;
                 $nameProduct3 = "potion";
                 $priceProduct3 = 5.20;
-            ?>
+
+                echo "<ul>
+               <li>Un $nameProduct1 coûte $priceProduct1 pièces d'or.</li>
+               <li>Une $nameProduct2 coûte $priceProduct2 pièces d'or.</li>
+               <li>Une $nameProduct3 coûte $priceProduct3 pièces d'or.</li>
+               </ul>"
+                    ?>
             </div>
         </section>
 
         <!-- QUESTION 3 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 3</h2>
-            <p class="exercice-txt">Calculer le montant total de la commande des produits ci-dessus avec les quantités ci-dessous et appliquez lui une remise de 10%.</p>
+            <p class="exercice-txt">Calculer le montant total de la commande des produits ci-dessus avec les quantités
+                ci-dessous et appliquez lui une remise de 10%.</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $quantityProduct1 = 1;
                 $quantityProduct2 = 10;
                 $quantityProduct3 = 4;
-            ?>
+
+
+                $totalPrice = ($priceProduct1 * $quantityProduct1 + $priceProduct2 * $quantityProduct2 + $priceProduct3 * $quantityProduct3);
+                $totalPriceTaxed = $totalPrice * 1.10;
+
+                echo "La commande est de $totalPrice pièces d'or et de $totalPriceTaxed pièces d'or avec les taxes.";
+                ?>
             </div>
         </section>
 
@@ -74,7 +90,29 @@
             <h2 class="exercice-ttl">Question 4</h2>
             <p class="exercice-txt">Affichez le prix le plus élevé des 3 produits ci-dessus.</p>
             <div class="exercice-sandbox">
-                
+                <?php
+                $mostExpensive = 0;
+                switch ($mostExpensive) {
+                    case $priceProduct1 > $priceProduct2 & $priceProduct3:
+                        echo $priceProduct1;
+                        break;
+                    case $priceProduct2 > $priceProduct1 & $priceProduct3:
+                        echo $priceProduct2;
+                        break;
+                    case $priceProduct3 > $priceProduct1 & $priceProduct2:
+                        echo $priceProduct3;
+                        break;
+                }
+
+                // if($priceProduct1 > $priceProduct2) {
+                //     $mostExpensive = $priceProduct1;
+                // } else {
+                //     $mostExpensive = $priceProduct2;
+                // }
+                // if ($priceProduct3 > $priceProduct1) {
+                //     $mostExpensive = $priceProduct3;
+                // } else
+                ?>
             </div>
         </section>
 
@@ -86,18 +124,20 @@
         ?>
         <section class="exercice">
             <h2 class="exercice-ttl">Question 5</h2>
-            <p class="exercice-txt">Affichez dans une liste HTML le nom des produits de la question 2 qui sont présents dans la phrase : "<?= $text1 ?>"</p>
+            <p class="exercice-txt">Affichez dans une liste HTML le nom des produits de la question 2 qui sont présents
+                dans la phrase : "<?= $text1 ?>"</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
         <!-- QUESTION 6 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 6</h2>
-            <p class="exercice-txt">Parmis les scores suivants, affichez le prénom des joueurs qui ont obtenus entre 50 et 150 points.</p>
+            <p class="exercice-txt">Parmis les scores suivants, affichez le prénom des joueurs qui ont obtenus entre 50
+                et 150 points.</p>
             <div class="exercice-sandbox">
-            <?php
+                <?php
                 $namePlayer1 = "Tim";
                 $scorePlayer1 = 67;
                 $namePlayer2 = "Morgan";
@@ -108,7 +148,7 @@
                 $scorePlayer4 = 134;
                 $namePlayer5 = "Kevin";
                 $scorePlayer5 = 103;
-            ?>
+                ?>
             </div>
         </section>
 
@@ -116,9 +156,10 @@
         <!-- QUESTION 7 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 7</h2>
-            <p class="exercice-txt">En réutilisant les scores de la question précédente, afficher le nom du joueur ayant obtenu le plus grand score.</p>
+            <p class="exercice-txt">En réutilisant les scores de la question précédente, afficher le nom du joueur ayant
+                obtenu le plus grand score.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
@@ -128,14 +169,15 @@
             <h2 class="exercice-ttl">Question 8</h2>
             <p class="exercice-txt">Affichez le prénom du joueur le plus long en nombre de caractères.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
         <!-- QUESTION 9 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 9</h2>
-            <p class="exercice-txt">Créer une variable $players ayant pour valeur un tableau multidimensionnel contenant toutes les données des joueurs avec leurs scores ci-dessus et leurs ages ci-dessous.</p>
+            <p class="exercice-txt">Créer une variable $players ayant pour valeur un tableau multidimensionnel contenant
+                toutes les données des joueurs avec leurs scores ci-dessus et leurs ages ci-dessous.</p>
             <ul class="exercice-txt">
                 <li>Tim : 25 ans</li>
                 <li>Morgan : 34 ans</li>
@@ -145,16 +187,17 @@
             </ul>
             <p class="exercice-txt">Afficher la valeur de cette variable avec tous les détails.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
 
         <!-- QUESTION 10 -->
         <section class="exercice">
             <h2 class="exercice-ttl">Question 10</h2>
-            <p class="exercice-txt">Afficher le prénom et l'âge du joueur le plus jeune dans une phrase dans une balise HTML P.</p>
+            <p class="exercice-txt">Afficher le prénom et l'âge du joueur le plus jeune dans une phrase dans une balise
+                HTML P.</p>
             <div class="exercice-sandbox">
-                
+
             </div>
         </section>
     </div>
