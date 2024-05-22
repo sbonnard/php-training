5daf91efe78f0869b691c648602d7f7e514eb7ab
Sébastien Bonnard <sebastien.bonnard94@gmail.com>
2024-05-22 16:25:52 +0200

    ex 5 done in exo3

--- app/exo3.php
+++ app/exo3.php
@@ -79,8 +79,8 @@ $prices = [3, 2, 2, 5, 8];
                 <ul>
                     <?php
                     $i = 0;
-                    foreach($store as $product => $price) {
-                        if ($price <= 4){
+                    foreach ($store as $product => $price) {
+                        if ($price <= 4) {
                             echo "<li>$product</li>";
                         }
                         $i++;
@@ -95,7 +95,17 @@ $prices = [3, 2, 2, 5, 8];
             <h2 class="exercice-ttl">Question 5</h2>
             <p class="exercice-txt">Afficher dans une liste HTML le nom des fruits ayant un prix pair</p>
             <div class="exercice-sandbox">
+                <ul>
+                    <?php
+                    $i = 0;
+                    foreach ($store as $fruit => $price) {
+                        if ($price % 2 === 0) {
+                            echo "<li>$fruit</li>";
+                        }
+                    }
 
+                    ?>
+                </ul>
             </div>
         </section>
 
